59a073c23a53a6bbb3568e9c519db93fdf8f598d
ABDULLAHRH1 <abdullahrh64@gmaiI.com>
2023-09-04 22:17:25 +0300

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
   </React.StrictMode> 
-=======
-  </React.StrictMode>
->>>>>>> 1e668fd1d2f935a18b772a6d88fe600c3c24f31d
 )
